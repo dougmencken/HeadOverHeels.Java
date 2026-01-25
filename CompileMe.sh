0eafbed749a8706791cf88f390741c91cbe1363b
Douglas Mencken <dougmencken@gmail.com>
2026-01-25 08:11:21 -0400

    bump the copyright year 2005 -> 2006
       find . -type f -print0 | xargs -0 sed -i 's/2025 Douglas/2026 Douglas/'
    
    in CompileMe.sh, show what the javac compiler is doing

--- CompileMe.sh
+++ CompileMe.sh
@@ -2,11 +2,15 @@
 
 [ -d com/github/trilarion/sound ] || ./submodules/CompileSubModules.sh
 
-javac -g -d . \
+javac -verbose -g -d . \
 	source/*.java \
 	source/items/*.java \
 	source/rooms/*.java \
 	source/behaviors/*.java \
 	source/gui/*.java \
 	source/gui/transitions/*.java \
-	source/gui/swing/*.java
+	source/gui/swing/*.java \
+2>&1 | grep --line-buffered -v '\[loading' | grep --line-buffered -v '\[parsing completed' \
+	| sed --unbuffered 's/\[parsing started/parsing/' \
+	| sed --unbuffered 's/[][]//g' \
+	| sed --unbuffered 's/SimpleFileObject//'
--- source/rooms/Miniature.java
+++ source/rooms/Miniature.java
@@ -1,6 +1,6 @@
 // The Java port of the free and open source remake of the game “Head over Heels”
 //
-// Copyright © 2025 Douglas Mencken dougmencken@gmail.com
+// Copyright © 2026 Douglas Mencken dougmencken@gmail.com
 //
 // This program is free software
 // You may redistribute it and~or modify it under the terms of the GNU General Public License
